78cd6fc54db6704eaf0d54890ef86d4ba44afab8
Michael Diente <michael.diente@englishcentral.com>
2019-11-11 01:24:46 +0800

    Working UTF8 1 byte implementation

---
+++ generator.js
@@ -0,0 +1,219 @@
+// Format: 
+// - start code point in decimal
+// - last code point in decimal
+// - number of bytes
+const utf8Ranges = [
+    [0, 127, 1], // 0000 - 007F, 0xxxxxxx
+    [128, 2047, 2], // 0080 - 07FF, 110xxxxx 10xxxxxx
+    [2048, 65535, 3], // 0800 - FFFF, 1110xxxx 10xxxxxx 10xxxxxx
+    [65536, 2097151, 4] // 10000 - 1FFFFF, 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+]
+
+/**
+ * Procedures:
+ * 1. Get what range the hex code falls into
+ * 2. 
+ */
+function computeUTF8(hexCode) {
+    console.log("computing %s to utf8", hexCode)
+    if ((hexCode.length % 2) == 1) {
+        console.log("hexCode size should be even", hexCode)
+        return false
+    }
+
+    let expectedNoOfBytes = 1
+    let computedRange = []
+    let computedBytes = [] // Should be [0, 1, 1, 0, 1]
+    const computedDecimal = hexToDecimal(hexCode)
+    console.log("computedDecimal: %o", computedDecimal)
+
+    // Get what range the hex code falls into
+    for (let i = 0;i < utf8Ranges.length;i++) {
+        const range = utf8Ranges[i]
+        const startDecimal = range[0]
+        const endDecimal = range[1]
+        if (computedDecimal >= startDecimal && endDecimal) {
+            computedRange = range
+            break
+        }
+    }
+    console.log("computedRange: %o", computedRange)
+    expectedNoOfBytes = computedRange[2]
+
+    let result = ""
+    for (let j = 0;j < hexCode.length;j+=2) {
+        let hex2Vals = hexCode.substring(j, j + 2)
+        let bit8FromDecimal = []
+
+        if (hex2Vals === "00") {
+            continue
+        }
+
+        for (let k = 0;k < 2;k++) {
+            let hex2Decimal = hexToDecimal(hex2Vals[k])
+            let bit4FromDecimal = decimalTo4bit(hex2Decimal)
+            bit8FromDecimal = bit8FromDecimal.concat(bit4FromDecimal)
+        }
+        
+        if (expectedNoOfBytes === 1) {
+            computedBytes.push(0)
+            
+            let startIndex = 8 - (8 - computedBytes.length)
+            while (computedBytes.length < 8) {
+                computedBytes.push(bit8FromDecimal[startIndex])
+                startIndex++
+            }
+            console.log("computedBytes: %o", computedBytes)
+            
+            let startComputedByteIndex = 4
+            while (startComputedByteIndex > -1) {
+                let bits = Object.assign([], computedBytes)
+                bits.splice(startComputedByteIndex, 4)
+
+                const hexLetter = bit4ToDecimal(bits)
+                result = result.concat(hexLetter)
+
+                console.log("bits: %o", bits)
+                console.log("hexLetter: %s", hexLetter)
+                startComputedByteIndex -= 4
+            }
+        }
+    }
+    console.log("result: %s", result)
+    return result
+}
+
+function computeUTF16(hexCode) {
+    console.log("computing %s to utf16", hexCode)
+    return "20AC"
+}
+
+function computeUTF32(hexCode) {
+    console.log("computing %s to utf32", hexCode)
+    return "000020AC"
+}
+
+function hexToDecimal(hexCode) {
+    let decimal = 0
+    const singleHexDecimals = [] // Should only have numbers
+    // Get decimal for each hex character
+    for (let i = 0;i < hexCode.length;i++) {
+        const singleHex = hexCode[i]
+        if (isValidHexNumber(singleHex)) {
+            singleHexDecimals.push(parseInt(singleHex))
+        } else if (isValidHexLetter(singleHex)) {
+            singleHexDecimals.push(hexLetterToDecimal(singleHex))
+        }
+    }
+    console.log("hexToDecimal values: %o", singleHexDecimals)
+    return singleHexDecimals.reduce((a, b) => a + b, 0)    
+}
+
+/**
+ * 
+ * @param {number} decimal - 0 - 15
+ */
+function decimalTo4bit(decimal) {
+    if (decimal > 15 || decimal < 0) {
+        return false
+    }
+
+    const expectedSize = 4
+    let quotient = decimal
+    let computedBits = [] 
+
+    while (quotient > 0) {
+        let remainder = parseInt(quotient % 2)
+        quotient =  parseInt(quotient / 2)
+        if (remainder != 0) {
+            remainder = 1
+        }
+        computedBits.push(remainder)
+    }
+    if (computedBits.length < expectedSize) {
+        const diff = expectedSize - computedBits.length
+        let incompleteBits = computedBits
+        incompleteBits.reverse()
+        for (let i = 0;i < diff;i++) {
+            incompleteBits.splice(0, 0, 0)
+        }
+        computedBits = incompleteBits.reverse()
+    }
+    return computedBits.reverse()
+}
+
+/**
+ * 
+ * @param {array} bits - E.g [1, 0, 0, 1]
+ */
+function bit4ToDecimal(bits) {
+    if (bits.length > 4 || bits.length < 4) {
+        return false
+    }
+    let startExp = bits.length - 1
+    let sum = 0
+    for (let i = 0;i < bits.length;i++) {
+        let multiplier = Math.pow(2, startExp)
+        let product = bits[i] * multiplier
+        startExp = startExp - 1
+        sum += product
+    }
+    return decimalToHexLetter(sum)
+}
+
+/**
+ * 
+ * @param {array} bits - if array sum is not 0. E.g [0, 0, 0] and sum is 0, return false
+ */
+function isValid8Bit(bits) {
+    return bits.reduce((a, b) => a + b, 0) > 0
+}
+
+function hexLetterToDecimal(letter) {
+    let decimal = 0
+    if (letter === "A") {
+        decimal = 10
+    } else if (letter === "B") {
+        decimal = 11
+    } else if (letter === "C") {
+        decimal = 12
+    } else if (letter === "D") {
+        decimal = 13
+    } else if (letter === "E") {
+        decimal = 14
+    } else if (letter === "F") {
+        decimal = 15
+    }
+    return decimal
+}
+
+function decimalToHexLetter(decimal) {
+    if (decimal == 10) {
+        return "A"
+    } else if (decimal == 11) {
+        return "B"
+    } else if (decimal == 12) {
+        return "C"
+    } else if (decimal == 13) {
+        return "D"
+    } else if (decimal == 14) {
+        return "E"
+    } else if (decimal == 15) {
+        return "F"
+    } else {
+        return decimal
+    }
+}
+
+function isValidHexNumber(number) {
+    return !isNaN(number)
+}
+
+function isValidHexLetter(character) {
+    return character === "A"
+        || character === "B"
+        || character === "C"
+        || character === "D"
+        || character === "E"
+        || character === "F"
+}
---
+++ unigen.js
@@ -0,0 +1,138 @@
+const modes = [
+    "UTF-8", 
+    "UTF-16", 
+    "UTF-32"
+]
+
+const hexCodes = [
+    ["1", "2", "3"],
+    ["4", "5", "6"],
+    ["7", "8", "9"],
+    ["A", "B", "C"],
+    ["D", "E", "F"]
+]
+
+const execBtns = [
+    "Convert", "Reset"
+]
+
+let selectedModePos = 0
+
+function loadViews() {
+    addModes()
+    addButtons()
+}
+
+function addButtons() {
+    const container = document.getElementById("hexbutton-view")
+    const table = document.createElement("table")
+    const tbody = document.createElement("tbody")
+    table.style.width = "100%"
+
+    for (let i = 0;i < hexCodes.length;i++) {
+        const hexCodeSet = hexCodes[i]
+        const tr = document.createElement("tr")
+
+        for (let j = 0;j < hexCodeSet.length;j++) {
+            const hexCode = hexCodes[i][j]
+            const td = document.createElement("td")
+            const button = document.createElement("button")
+            button.innerHTML = hexCode
+            button.className = "hex-code-btn"
+            td.appendChild(button)
+            tr.appendChild(td)
+
+            // Convert button
+            if (i == 0 && j == hexCodeSet.length - 1) {
+                const label = execBtns[0]
+                const eTd = document.createElement("td")
+                const eBtn = document.createElement("button")
+                eBtn.className = "convert-btn"
+                eBtn.innerHTML = label
+                eTd.appendChild(eBtn)
+                tr.appendChild(eTd)
+            }
+
+            // Reset button
+            if (i == hexCodes.length - 1 && j == hexCodeSet.length - 1) {
+                const label = execBtns[1]
+                const eTd = document.createElement("td")
+                const eBtn = document.createElement("button")
+                eBtn.className = "reset-btn"
+                eBtn.innerHTML = label
+                eTd.appendChild(eBtn)
+                tr.appendChild(eTd)
+            }
+        }
+        tbody.appendChild(tr)
+    }
+    table.appendChild(tbody)
+    container.appendChild(table)
+}
+
+function addModes() {
+    const container = document.getElementById("mode-view")
+    const table = document.createElement("table")
+    const tbody = document.createElement("tbody")
+    const tr = document.createElement("tr")
+    table.style.width = "100%"
+
+    for (let i = 0;i < modes.length;i++) {
+        const mode = modes[i]
+        const td = document.createElement("td")
+        const button = document.createElement("button")
+        button.id = "modeBtn" + i
+        button.value = i
+        button.innerHTML = mode
+        button.addEventListener("click", onClickMode)
+
+        if (i == selectedModePos) {
+            button.className = "mode-selected-btn"
+        } else {
+            button.className = "mode-unselected-btn"
+        }
+
+        td.appendChild(button)
+        tr.appendChild(td)
+        tbody.appendChild(tr)
+    }
+    table.appendChild(tbody)
+    container.appendChild(table)
+}
+
+function refreshModes() {
+    for (let i = 0;i < modes.length;i++) {
+        const button = document.getElementById("modeBtn" + i)
+        if (i == selectedModePos) {
+            button.className = "mode-selected-btn"
+        } else {
+            button.className = "mode-unselected-btn"
+        }
+    }
+}
+
+function onClickMode(ref) {
+    const view = ref.target
+    const inputHexCodeView = document.getElementById("input-hexcode")
+    const position = view.value
+    const inputHexCode = inputHexCodeView.innerHTML.replace("U+", "")
+
+    selectedModePos = position
+
+    let result = ""
+    if (position == 0) {
+        result = computeUTF8(inputHexCode)
+    } else if (position == 1) {
+        result = computeUTF16(inputHexCode)
+    } else {
+        result = computeUTF32(inputHexCode)
+    }
+
+    showResult("U+" + result)
+    refreshModes()
+}
+
+function showResult(resultInString) {
+    const output = document.getElementById("output-hexcode")
+    output.innerHTML = resultInString
+}
